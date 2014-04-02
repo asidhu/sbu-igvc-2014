a0a8fbbb9df762dec51af649deee531aa211dc2c
asidhu <infinitegde@gmail.com>
2014-04-02 18:57:13 -0400

    fixed install.sh

--- install.sh
+++ install.sh
@@ -25,18 +25,18 @@ sudo apt-get -y install libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-to
 
 echo "Getting x264..."
 sudo mkdir -p $X264_LOCATION
-sudo chown sbrt $X264_LOCATION
+sudo chown $USER $X264_LOCATION
 cd $X264_LOCATION
 wget ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20120528-2245-stable.tar.bz2
 tar xvf x264-snapshot-20120528-2245-stable.tar.bz2
-cd x264-snapshot-20120528-2245-stable.tar.bz2
+cd x264-snapshot-20120528-2245-stable
 ./configure --enable-shared --enable-pic
 make
 sudo make install
 
 echo "Getting V4L..."
 sudo mkdir -p $V4L_LOCATION
-sudo chown sbrt $V4L_LOCATION
+sudo chown $USER $V4L_LOCATION
 cd $V4L_LOCATION
 wget http://www.linuxtv.org/downloads/v4l-utils/v4l-utils-0.8.8.tar.bz2
 tar xvf v4l-utils-0.8.8.tar.bz2
@@ -46,9 +46,9 @@ sudo make install
 
 echo "Installing opencv..."
 sudo mkdir -p $OPENCV_LOCATION
-sudo chown sbrt $OPENCV_LOCATION
+sudo chown $USER $OPENCV_LOCATION
 cd $OPENCV_LOCATION
-git clone https://github.com/Itseez/opencv.git
+git clone https://github.com/Itseez/opencv.git .
 mkdir build
 cd build 
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
